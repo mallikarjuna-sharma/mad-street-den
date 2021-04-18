7cbde706ca802bd1b2400db530a232f00129c7ec
mallikarjuna sharma <mallikarjuna.s@mobinius.com>
2021-04-18 23:50:31 +0530

    compression image slide show

--- src/pages/Desktop.jsx
+++ src/pages/Desktop.jsx
@@ -6,14 +6,14 @@ import ContextMenu from "../components/CustomizedMenus";
 import CustomizedMenus from "../components/CustomizedMenus";
 import SlideShow from "../components/SlideShow/SlideShow";
 
-import image1 from "../assets/1.jpg";
-import image2 from "../assets/2.jpg";
-import image3 from "../assets/3.jpg";
-import image4 from "../assets/4.jpg";
-import image5 from "../assets/5.jpg";
-import image6 from "../assets/6.jpg";
-import image7 from "../assets/7.jpg";
-import image8 from "../assets/8.jpg";
+import image1 from "../assets/1-compressed.jpg";
+import image2 from "../assets/2-compressed.jpg";
+import image3 from "../assets/3-compressed.jpg";
+import image4 from "../assets/4-compressed.jpg";
+import image5 from "../assets/5-compressed.jpg";
+import image6 from "../assets/6-compressed.jpg";
+import image7 from "../assets/7-compressed.jpg";
+import image8 from "../assets/8-compressed.jpg";
 import { Redirect, useHistory } from "react-router-dom";
 import { getlocalStorage, getlUrlPaths, SIGNED_IN_USER } from "../utils";
 
